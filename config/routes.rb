730c12c161bc710a167bf1b0930cf5020c2542ae
mantanquzzmo <quzzmo@hotmail.com>
2020-06-11 15:29:17 -0700

    sets up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'api/auth', skip: [:omniauth_callbacks]
+  namespace :api, defaults: { format: :json } do
+      # resources :teams, only: [:create, :index, :destroy]
+      # resources :players, only: [:create, :index, :show, :destroy, :update]
+  end
 end
