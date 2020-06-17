7371d121e7da9f61d83e4f36aa0f9e1babc23969
mantanquzzmo <quzzmo@hotmail.com>
2020-06-16 20:19:45 -0700

    builds further index action

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,7 +1,10 @@
 class Api::TeamsController < ApplicationController
   def index
+    binding.pry
     teams = Team.all
     render json: teams, status: 200
+
+
   end
 end
 
--- spec/requests/api/teams/index_spec.rb
+++ spec/requests/api/teams/index_spec.rb
@@ -2,12 +2,32 @@
 
 RSpec.describe 'GET /api/teams', type: :request do
   describe 'Teams index call' do
-      let!(:team) { create_list(:team, 3) }
+    let!(:team) { create_list(:team, 3) }
+    let(:user) { create(:user) }
+    let(:credentials) { user.create_new_auth_token }
+    let!(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
 
-    it 'succesfully presents the teams to the user' do
+    it 'succesfully presents teams to the user when not signed in' do
       get '/api/teams'
-
       expect(response_json.length).to eq 3
+    end
+
+    it 'and returns a 200 response status' do
+      expect(response).to have_http_status 200
+    end
+
+    describe 'list teams belonging to that user' do
+      before do
+        get '/api/teams', headers: headers
+      end
+
+      it 'and returns a 200 response status' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'and returns 3 teams that belong to player' do
+        expect(response_json.length).to eq 2
+      end
     end
   end
 end
