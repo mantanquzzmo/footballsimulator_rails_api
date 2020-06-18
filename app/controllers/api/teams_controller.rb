af418cb688016a8d9173ce047a93c3c12ab555f5
mantanquzzmo <quzzmo@hotmail.com>
2020-06-17 21:24:46 -0700

    renders different responses for index action if user is logged in

--- app/controllers/api/teams_controller.rb
+++ app/controllers/api/teams_controller.rb
@@ -1,10 +1,16 @@
 class Api::TeamsController < ApplicationController
   def index
-    binding.pry
-    teams = Team.all
-    render json: teams, status: 200
 
+    teams_to_display = []
 
+    if current_user
+      teams = Team.where(user_id: current_user.id)
+      teams_to_display << teams
+    else
+      teams = Team.all
+      teams_to_display << teams
+    end
+    render json: teams_to_display[0], status: 200
   end
 end
 
--- spec/requests/api/teams/index_spec.rb
+++ spec/requests/api/teams/index_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe 'GET /api/teams', type: :request do
   describe 'Teams index call' do
-    let!(:team) { create_list(:team, 3) }
+    let!(:team) { create_list(:team, 3, user_id: user.id) }
     let(:user) { create(:user) }
     let(:credentials) { user.create_new_auth_token }
     let!(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
@@ -26,7 +26,7 @@ RSpec.describe 'GET /api/teams', type: :request do
       end
 
       it 'and returns 3 teams that belong to player' do
-        expect(response_json.length).to eq 2
+        expect(response_json.length).to eq 3
       end
     end
   end
