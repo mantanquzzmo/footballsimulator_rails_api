fbbe011f639717455f024c3f58ac7a4289012587
mantanquzzmo <quzzmo@hotmail.com>
2020-07-19 17:21:57 -0700

    generates opponent + teams

--- app/controllers/api/seasons_controller.rb
+++ app/controllers/api/seasons_controller.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 class Api::SeasonsController < ApplicationController
-  include TeamsCreator
+  include TeamsCreator, PlayersCreator
 
   def create
     :authenticate_user!
-    # Create computer teams return user team + computer teams
-    #   team_name = team_name_generator(i)
 
-    #   team = Team.create(name: params[:name],
-    #               primary_color: params[:primary_color],
-    #               secondary_color: params[:secondary_color],
-    #               user_id: current_user.id)
-    # end
+    cpu_opponent = User.create(email: 'cpu_opponent' + rand(0..1000).to_s + '@mail.com', password: 'password')
+    player_team = Team.where(id: params[:team_id])
+    season_teams = [player_team[0]]
+
+    (0..4).each do |i|
+      team_name = team_name_generator(i)
+      team = Team.create(name: team_name,
+                         primary_color: 'white',
+                         secondary_color: 'red',
+                         user_id: cpu_opponent.id)
+
+      if team.persisted?
+        players = generate_players(team.id)
+        season_teams << team
+      else
+        render json: { error: team.errors.full_messages }, status: 422
+      end
+    end
   end
 end
--- spec/factories/cpu_teams.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :cpu_team do
-    name { 'Brazil' }
-    primary_color { 'Yellow' }
-    secondary_color { 'Green' }
-  end
-end
