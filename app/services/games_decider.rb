885a5da7c541839ddeb67e35858e3bfa3f198e7e
mantanquzzmo <quzzmo@hotmail.com>
2020-07-27 17:25:02 -0700

    adding to games_decider

--- app/services/games_decider.rb
+++ app/services/games_decider.rb
@@ -1,5 +1,30 @@
 module GamesDecider
   def games_decider(games)
-    binding.pry
+
+    games.each {|game|
+    hometeam = game.teams[0]
+    awayteam = game.teams[1]
+
+    ## calculate each players value
+
+    ## calculate game result
+
+    ## make copy and insert player performance
+
+    ## update original player new form
+
+    
+
+    ht_gk = hometeam.players.where(starting_11: true, position: "G")
+    at_gk = awayteam.players.where(starting_11: true, position: "G")
+    ht_def = hometeam.players.where(starting_11: true, position: "D")
+    at_def = awayteam.players.where(starting_11: true, position: "D")
+    ht_mid = hometeam.players.where(starting_11: true, position: "M")
+    at_mid = awayteam.players.where(starting_11: true, position: "M")
+    ht_att = hometeam.players.where(starting_11: true, position: "A")
+    at_att = awayteam.players.where(starting_11: true, position: "A")
+    
+  }
+
   end
 end
